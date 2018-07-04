605fe0d9e5aed0d816d0572c3e731f8f61319221
SethDonohue <donohue.seth@gmail.com>
2018-07-04 13:35:53 -0700

    correctly configured eslint and airbnb

--- highlighted-phrases/src/lib/span-wrapper.js
+++ highlighted-phrases/src/lib/span-wrapper.js
@@ -1,23 +1,20 @@
 // This module will grab the article text and wrap each word in a span
+const allArticleParagraphs = document.querySelectorAll('.article-paragraph-target');
 
-  const allArticleParagraphs = document.querySelectorAll('.article-paragraph-target');
 
+const wordsWrapper = (paragraphNodeList) => {
+  // get the text inside the element
+  const paragraphWords = [];
+  paragraphNodeList.forEach(paragraph => {
+    // split text into words array
+    const words = paragraph.textContent.split(' ');
 
-  const wordsWrapper = (paragraphNodeList) => {
-    // get the text inside the element
-    let paragraphWords = [];
-    paragraphNodeList.forEach(paragraph => {
-      //split text into words array
-      const words = paragraph.textContent.split(' ');
+    // wrap each word in a <span>
+    const wrappedWords = words.map(word => `<span>${word}</span>`).join(' ');
 
-      // wrap each word in a <span>
-      const wrappedWords = words.map(word => `<span>${word}</span>`).join(' ');
+    // set as innerHTML for paragraph?
+    paragraph.innerHTML = wrappedWords;
+  });
+};
 
-      // set as innerHTML for paragraph?
-      paragraph.innerHTML = wrappedWords;
-      console.log(paragraphNodeList);
-    });
-      
-  }
-
-  wordsWrapper(allArticleParagraphs);
+wordsWrapper(allArticleParagraphs);
