4d9b59a4789aa51cd9947cc876eb2069effed27a
SethDonohue <donohue.seth@gmail.com>
2018-07-04 13:27:38 -0700

    each word succesfully wrapped in a span

--- highlighted-phrases/src/lib/span-wrapper.js
+++ highlighted-phrases/src/lib/span-wrapper.js
@@ -5,19 +5,19 @@
 
   const wordsWrapper = (paragraphNodeList) => {
     // get the text inside the element
-  let paragraphWords = [];
-  paragraphNodeList.forEach(paragraph => {
-    //split text into words array
-    const words = paragraph.textContent.split(' ');
-    console.log(words);
-    // wrap each word in a <span>
+    let paragraphWords = [];
+    paragraphNodeList.forEach(paragraph => {
+      //split text into words array
+      const words = paragraph.textContent.split(' ');
 
-    // join backtogether
-  
-    // set as innerHTML for paragraph?
+      // wrap each word in a <span>
+      const wrappedWords = words.map(word => `<span>${word}</span>`).join(' ');
 
-
-  });
+      // set as innerHTML for paragraph?
+      paragraph.innerHTML = wrappedWords;
+      console.log(paragraphNodeList);
+    });
+      
   }
 
   wordsWrapper(allArticleParagraphs);
