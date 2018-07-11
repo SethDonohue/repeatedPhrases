4d54a7990a0d089aa23abc27372eb57a42114a44
SethDonohue <donohue.seth@gmail.com>
2018-07-11 14:08:13 -0700

    added second work list for test

--- highlighted-phrases/src/main.js
+++ highlighted-phrases/src/main.js
@@ -5,7 +5,7 @@ import Controller from './lib/control';
 console.log('Hello world...');
 
 // Test Input
-const testWordLists = {
+const originalLists = {
   'red-list': [
     'action-oriented',
     'alarming',
@@ -18,10 +18,45 @@ const testWordLists = {
     'creative',
     'love',
     'new technology',
+  ],
+  'blue-list': [
+    'an adorable puppy',
+    'aggressive',
+    'arm',
+    'very unlikely',
+  ],
+  'purple-list': [
+    'do not cross',
+    'log file',
+    'our team',
+    'radio',
+  ],
+  'grey-list': [
+    'very unlikely to leave',
+    'will deliver new',
+  ],
+};
+
+const testWordLists = {
+  'red-list': [
+    'action-oriented',
+    'alarming',
+    'candidates',
+    'leave',
+    'do not want',
+    'actionable',
+  ],
+  'green-list': [
+    'adorable',
+    'creative',
+    'love',
+    'new technology',
     'groundbreaking',
   ],
   'blue-list': [
     'an adorable puppy',
+    'actionable item an puppy',
+    'an actionable item puppy',
     'aggressive',
     'arm',
     'very unlikely',
@@ -42,18 +77,18 @@ const testString = 'I\'m very unlikely to leave an adorable puppy. We do not wan
 
 const testStringTwo = 'We expect our candidates to be action-oriented, aggressive and have creative ideas for our team. You will deliver new technology and groundbreaking designs.';
 
-const shortTest = ' puppy an adorable. adorable an puppy. adorable puppy adorable. puppy. an adorable puppy.... adorable an puppy. puppy an adorable';
+const shortTest = ' puppy an adorable. adorable an puppy. adorable puppy adorable. puppy. an adorable puppy.... adorable an puppy. puppy an adorable. an actionable item puppy actionable item an puppy';
 const shortTestTwo = 'very unlikely to leave very unlikely to! leave very unlikely to. leave very unlikely to? leave leave very unlikely to. leave very unlikely to? unlikey to leave very. leave very unlikely to';
 const shortTestThree = 'our arm';
 
-// const controllerOne = new Controller('highlighter-target', testWordLists);
-// controllerOne.renderHighlights(testString);
+const controllerOne = new Controller('highlighter-target', testWordLists);
+controllerOne.renderHighlights(testString);
 
-// const controllerTwo = new Controller('highlighter-target-two', testWordLists);
-// controllerTwo.renderHighlights(testStringTwo);
+const controllerTwo = new Controller('highlighter-target-two', testWordLists);
+controllerTwo.renderHighlights(testStringTwo);
 
-// const controllerThree = new Controller('highlighter-target-three', testWordLists);
-// controllerThree.renderHighlights(shortTestTwo);
+const controllerThree = new Controller('highlighter-target-three', testWordLists);
+controllerThree.renderHighlights(shortTestTwo);
 
 const controllerFour = new Controller('highlighter-target-four', testWordLists);
 controllerFour.renderHighlights(shortTest);
